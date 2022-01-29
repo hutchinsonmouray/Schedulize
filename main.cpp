b4ef0595996fe50e47cbf18249448afd273a30b6
hutchinsonmouray <80347718+hutchinsonmouray@users.noreply.github.com>
2022-01-28 20:06:33 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "schedule.cpp"
+#include "schedule.h"
+
+using namespace std;
+
+int main() {
+    Schedulize obj;
+    obj.tasks; //vector of nodes
+
+}
---
+++ schedule.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "schedule.h"
+using namespace std;
+
+//node structure --
+struct node {
+    //unsigned int date //due date
+
+    //below is all strings
+    //descrip //description DESCRIPTION: -> LOCATION:
+    //title  SUMMARY: -> [
+    //course - [ART1010C]
+    //
+
+};
+
+//assignment class
+class Schedulize {
+
+    void reader() {
+        //reader function
+        //reads the file
+        //creates a vector of node
+    }
+
+    void dateSort();
+    //sort nodes by date
+
+    //sort by time & date;
+
+    void classSort(string courseCode);
+    //display based by class
+
+public:
+    vector<node>tasks;
+};
