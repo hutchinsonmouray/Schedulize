916603d54553df529901ad223b2da3b1dfe61586
jonathan1q2w3e4r <jonathanlevy@gmail.com>
2022-01-28 22:30:23 -0500

    buahahah

--- schedule.cpp
+++ schedule.cpp
@@ -43,7 +43,7 @@ public:
 
     //sort nodes by date
     void dateSort() {
-        cout < "I created functionality buahahaha";
+        cout << "I created functionality buahahaha";
     };
     //sort by length;
 
